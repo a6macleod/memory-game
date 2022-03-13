ef199144026c09563f3cfbb130c9e7ac917f1cdc
Andrew MacLeod <A6macleod@gmail.com>
2022-03-12 19:33:36 -0500

    refactor Gameboard to map out cards

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,82 +1,17 @@
 import React from "react";
 
-const Cards = (props) => {
+const Cards = ({ allCards, checkIfClicked }) => {
   return (
     <div className="Cards-section">
       <div className="cards-container">
-        <div
-          className="card 0"
-          onClick={() => props.checkIfClicked(props.allCards[0])}
-        >
-          <img src={props.allCards[0].img} alt="gears" />
-        </div>
-        <div
-          className="card 1"
-          onClick={() => props.checkIfClicked(props.allCards[1])}
-        >
-          <img src={props.allCards[1].img} alt="gears" />
-        </div>
-        <div
-          className="card 2"
-          onClick={() => props.checkIfClicked(props.allCards[2])}
-        >
-          <img src={props.allCards[2].img} alt="gears" />
-        </div>
-        <div
-          className="card 3"
-          onClick={() => props.checkIfClicked(props.allCards[3])}
-        >
-          <img src={props.allCards[3].img} alt="gears" />
-        </div>
-        <div
-          className="card 4"
-          onClick={() => props.checkIfClicked(props.allCards[4])}
-        >
-          <img src={props.allCards[4].img} alt="gears" />
-        </div>
-        <div
-          className="card 5"
-          onClick={() => props.checkIfClicked(props.allCards[5])}
-        >
-          <img src={props.allCards[5].img} alt="gears" />
-        </div>
-        <div
-          className="card 6"
-          onClick={() => props.checkIfClicked(props.allCards[6])}
-        >
-          <img src={props.allCards[6].img} alt="gears" />
-        </div>
-        <div
-          className="card 7"
-          onClick={() => props.checkIfClicked(props.allCards[7])}
-        >
-          <img src={props.allCards[7].img} alt="gears" />
-        </div>
-        <div
-          className="card 8"
-          onClick={() => props.checkIfClicked(props.allCards[8])}
-        >
-          <img src={props.allCards[8].img} alt="gears" />
-        </div>
-
-        <div
-          className="card 9"
-          onClick={() => props.checkIfClicked(props.allCards[9])}
-        >
-          <img src={props.allCards[9].img} alt="gears" />
-        </div>
-        <div
-          className="card 10"
-          onClick={() => props.checkIfClicked(props.allCards[10])}
-        >
-          <img src={props.allCards[10].img} alt="gears" />
-        </div>
-        <div
-          className="card 11"
-          onClick={() => props.checkIfClicked(props.allCards[11])}
-        >
-          <img src={props.allCards[11].img} alt="gears" />
-        </div>
+        {allCards.map((card) =>
+          <div
+            className="card"
+            onClick={() => checkIfClicked(card)}
+          >
+            <img src={card.img} alt="gears" />
+          </div>
+        )}
       </div>
     </div>
   );
